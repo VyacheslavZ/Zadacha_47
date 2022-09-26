6a396503364628795708833d7463644944a9d8cc
Slava <d-color.38@mail.ru>
2022-09-26 22:19:42 +0800

    dz47

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿double[,] FillMas(int n, int m)
+{
+    double[,] mas = new double[n, m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            mas[i, j] = new Random().Next(-100, 101);
+            mas[i, j] /= 10;
+        }
+    }
+    return mas;
+}
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (j != arr.GetLength(1) - 1)
+                Console.Write($"|{arr[i, j]}|, ");
+            else if (i == arr.GetLength(0) - 1 && j == arr.GetLength(1) - 1)
+                Console.WriteLine($"|{arr[i, j]}|");
+            else if (j == arr.GetLength(1) - 1)
+                Console.WriteLine($"|{arr[i, j]}|,");
+        }
+    }
+}
+
+System.Console.Write("Введите количество строк n = ");
+int n = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите количество столбцов m = ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] newArray = FillMas(n, m);
+PrintArray(newArray);
